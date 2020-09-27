ae4a338a2f66cc7c0b863e702d28c0c746b15fd4
dsmirnov <dsmirnov@centerfield.com>
2020-09-27 16:37:10 +0300

    Verstka

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-import './module'
+import {Excel} from '@/components/excel/Excel';
 import './scss/index.scss'
+import {Formula} from '@/components/formula/Formula';
+import {Header} from '@/components/header/Header';
+import {Toolbar} from '@/components/toolbar/Toolbar';
+import {Table} from '@/components/table/Table';
 
-console.log('Working!')
+const excel = new Excel('#app', {
+	components: [Header, Toolbar, Formula, Table]
+})
+
+excel.render()
